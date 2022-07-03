96bc1839533df3555d963ce5b75ddcc0f477c240
Konrad Borowski <konrad@borowski.pw>
2022-07-03 11:07:53 +0200

    Remove frameset usage

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: '/live/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        edit: resolve(__dirname, "edit.html"),
-      },
-    },
-  },
-});
+export default defineConfig({ base: "/live/" });
